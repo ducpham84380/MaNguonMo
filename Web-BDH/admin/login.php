d013e24629490b7b85869d9b2d98511ab6ba9c69
luuthoaihung <hunggokai@gmail.com>
2023-04-05 16:38:20 +0700

    your commit message

---
+++ Web-BDH/admin/login.php
@@ -0,0 +1,89 @@
+<?php
+	session_start();
+ include('../db/connect.php'); 
+?>
+<?php
+	// session_destroy();
+	// unset('dangnhap');
+	if(isset($_POST['dangnhap'])) {
+		$taikhoan = $_POST['taikhoan'];
+		$matkhau = md5($_POST['matkhau']);
+		if($taikhoan=='' || $matkhau ==''){
+			echo '<p>Xin nhập đủ</p>';
+		}else{
+			$sql_select_admin = mysqli_query($con,"SELECT * FROM tbl_admin WHERE email='$taikhoan' AND password='$matkhau' LIMIT 1");
+			$count = mysqli_num_rows($sql_select_admin);
+			$row_dangnhap = mysqli_fetch_array($sql_select_admin);
+			if($count>0){
+				$_SESSION['dangnhap'] = $row_dangnhap['admin_name'];
+				$_SESSION['admin_id'] = $row_dangnhap['admin_id'];
+				header('Location: dashboard.php');
+			}else{
+				echo '<p>Tài khoản hoặc mật khẩu sai</p>';
+			}
+		}
+	}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin - Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+
+  <div class="container">
+    <div class="card card-login mx-auto mt-5">
+      <div class="card-header">Login</div>
+      <div class="card-body">
+      <form action="" method="POST">
+          <div class="form-group">
+            <div class="form-label-group">
+            <input type="email" id="inputEmail" name="taikhoan" class="form-control" placeholder="Email address" required="required" autofocus="autofocus">
+           
+              <label for="inputEmail">Email address</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-label-group">
+            <input type="password" name="matkhau" id="inputPassword" class="form-control" placeholder="Password" required="required">
+           
+              <label for="inputPassword">Password</label>
+            </div>
+          </div>
+          
+          <input class="btn btn-primary btn-block" type="submit" name="dangnhap" value="Đăng Nhập">
+         
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+</body>
+
+</html>
